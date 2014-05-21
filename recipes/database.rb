2d6bf16028085472c7d6c73cd90579d26c3311fb
Sean Escriva <sean.escriva@gmail.com>
2014-05-21 11:00:41 -0400

    use mysql_service resource and set strict for tables

--- recipes/database.rb
+++ recipes/database.rb
@@ -1,3 +1,6 @@
+# Cookbook Name:: phabricator
+# Recipe:: database
+
 # Copyright (C) 2014  Sean Escriva
 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +15,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include_recipe "mysql::server"
 include_recipe "mysql::client"
 
 node.set['phabricator']['config']['mysql.pass'] = node['mysql']['server_root_password']
+
+mysql_service node['mysql']['service_name'] do
+  port node['mysql']['port']
+  data_dir node['mysql']['data_dir']
+  server_root_password node['mysql']['server_root_password']
+  server_debian_password node['mysql']['server_debian_password']
+  server_repl_password node['mysql']['server_repl_password']
+  allow_remote_root node['mysql']['allow_remote_root']
+  remove_anonymous_users node['mysql']['remove_anonymous_users']
+  remove_test_database node['mysql']['remove_test_database']
+  root_network_acl node['mysql']['root_network_acl']
+  action :create
+end
+
+service "mysql" do
+  action :nothing
+end
+
+file "#{node['phabricator']['mysql_conf_d']}/strict_all_tables.cnf" do
+  content "[mysqld]\nsql_mode=STRICT_ALL_TABLES\n"
+  notifies :restart, "service[mysql]", :delayed
+end
